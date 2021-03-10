09ae9f2d1df2904a8a366b510a0f0f4826333d6a
Guohui Xiao <xiao@inf.unibz.it>
2021-03-11 00:14:10 +0100

    update examples to use ODH

--- java/src/main/java/org/ghxiao/ontop/EndpointDemo.java
+++ java/src/main/java/org/ghxiao/ontop/EndpointDemo.java
@@ -10,18 +10,22 @@ import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 
 public class EndpointDemo {
     public static void main(String[] args) {
-        String sparqlEndpoint = "http://testbed.inode.igd.fraunhofer.de:18000/sparql";
+        String sparqlEndpoint = "https://sparql.opendatahub.bz.it/sparql";
         Repository repo = new SPARQLRepository(sparqlEndpoint);
 
         try (RepositoryConnection conn = repo.getConnection()) {
-            String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                    "SELECT * WHERE {\n" +
-                    "  ?project a <http://unics.cloud/ontology#Project> ; \n" +
-                    "       <http://unics.cloud/ontology#year> ?y;\n" +
-                    "\t  <http://unics.cloud/ontology#name> ?name .\n" +
-                    "} \n" +
-                    "LIMIT 1000";
+            String queryString = "PREFIX schema: <http://schema.org/>\n" +
+                    "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
+                    "\n" +
+                    "SELECT ?h ?pos ?posLabel ?posColor\n" +
+                    "WHERE {\n" +
+                    "  ?h a schema:LodgingBusiness ;\n" +
+                    "     geo:asWKT ?pos ;\n" +
+                    "     schema:name ?posLabel ;\n" +
+                    "     schema:address ?a .\n" +
+                    "  # ?a schema:postalCode \"39100\" . # Uncomment for Bolzano only\n" +
+                    "FILTER (lang(?posLabel) = 'de')}" +
+                    "LIMIT 100 ";
             TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
             //TupleQueryResult result = tupleQuery.evaluate();
             try (TupleQueryResult result = tupleQuery.evaluate()) {
--- py/src/demo.py
+++ py/src/demo.py
@@ -1,14 +1,19 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-sparql = SPARQLWrapper("http://testbed.inode.igd.fraunhofer.de:18000/sparql")
+sparql = SPARQLWrapper("https://sparql.opendatahub.bz.it/sparql")
 q = """
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-  ?project a <http://unics.cloud/ontology#Project> ; 
-       <http://unics.cloud/ontology#year> ?y;
-	  <http://unics.cloud/ontology#name> ?name .
-} 
-LIMIT 1000
+PREFIX schema: <http://schema.org/>
+PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+PREFIX : <http://noi.example.org/ontology/odh#>
+
+SELECT ?pos ?posLabel
+WHERE {
+  ?p a :Pizzeria ;
+     geo:asWKT ?pos ;
+     schema:name ?posLabel ;
+     schema:geo ?geo .
+  FILTER (lang(?posLabel) = 'it')
+  }
+  LIMIT 100
 """
 sparql.setQuery(q)
 sparql.setReturnFormat(JSON)
